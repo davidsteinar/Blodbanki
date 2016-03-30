079e19d442579e92172e7cef7d0b9477163c05c0
davidsteinar <davidsteinar@gmail.com>
2016-03-30 21:57:24 +0200

    Probability distributions

--- BlodbankiSim/BlodbankiSim/BlodbankiSim.c
+++ BlodbankiSim/BlodbankiSim/BlodbankiSim.c
@@ -2,10 +2,56 @@
 //
 
 #include "stdafx.h"
+#include <math.h>
+#include <stdlib.h>
 
+
+int  poissonRandom(double expectedValue);
+double  normalDistribution(double mu, double sigma);
 
 int main()
 {
     return 0;
 }
 
+
+double normalDistribution(double mu, double sigma)
+{
+	double U1, U2, W, mult;
+	static double X1, X2;
+	static int call = 0;
+
+	if (call == 1)
+	{
+		call = !call;
+		return (mu + sigma * (double)X2);
+	}
+
+	do
+	{
+		U1 = -1 + ((double)rand() / RAND_MAX) * 2;
+		U2 = -1 + ((double)rand() / RAND_MAX) * 2;
+		W = pow(U1, 2) + pow(U2, 2);
+	} while (W >= 1 || W == 0);
+
+	mult = sqrt((-2 * log(W)) / W);
+	X1 = U1 * mult;
+	X2 = U2 * mult;
+
+	call = !call;
+
+	return (mu + sigma * (double)X1);
+}
+
+int poissonRandom(double expectedValue) {
+	int n = 0; //counter of iteration
+	double limit;
+	double x;  //pseudo random number
+	limit = exp(-expectedValue);
+	x = rand() / INT_MAX;
+	while (x > limit) {
+		n++;
+		x *= rand() / INT_MAX;
+	}
+	return n;
+}
